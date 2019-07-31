a2f606982cd33b15f68185c60ae873b71f00eb04
Liran Wang <e0385291@u.nus.edu>
2019-07-31 15:55:23 +0800

    update catalogueDAO

--- SSISTeam9/DAO/CatalogueDAO.cs
+++ SSISTeam9/DAO/CatalogueDAO.cs
@@ -9,7 +9,7 @@ namespace SSISTeam9.DAO
 {
     public class CatalogueDAO
     {
-        public List<Inventory> DisplayAllCatalogue()
+        public static List<Inventory> DisplayAllCatalogue()
         {
             List<Inventory> catalogues = new List<Inventory>();
 
@@ -27,14 +27,14 @@ namespace SSISTeam9.DAO
                     {
                         ItemId = (int)reader["itemId"],
                         ItemCode = (string)reader["itemCode"],
-                        BinNo = (string)reader["binNo"],
+                        BinNo = (reader["binNo"] == DBNull.Value) ? "Nil" : (string)reader["binNo"],
                         StockLevel = (int)reader["stockLevel"],
                         ReorderLevel = (int)reader["reorderLevel"],
                         ReorderQty = (int)reader["reorderQty"],
                         Category = (string)reader["category"],
                         Description = (string)reader["description"],
                         UnitOfMeasure = (string)reader["unitOfMeasure"],
-                        ImageUrl = (string)reader["imageUrl"]
+                        ImageUrl = (reader["imageUrl"] == DBNull.Value) ? "Nil" : (string)reader["imageUrl"]
                     };
                     catalogues.Add(catalogue);
                 }
@@ -43,10 +43,10 @@ namespace SSISTeam9.DAO
         }
 
 
-        public Inventory DisplaySelectedCatalogue(int ItemId)
+        public static Inventory DisplaySelectedCatalogue(int ItemId)
         {
 
-            Inventory catalogue = new Inventory();
+            Inventory catalogue = null;
 
             using (SqlConnection conn = new SqlConnection(Data.db_cfg))
             {
@@ -58,23 +58,60 @@ namespace SSISTeam9.DAO
                 SqlDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    catalogue.ItemId = (int)reader["itemId"];
-                    catalogue.ItemCode = (string)reader["itemCode"];
-                    catalogue.BinNo = (string)reader["binNo"];
-                    catalogue.StockLevel = (int)reader["stockLevel"];
-                    catalogue.ReorderLevel = (int)reader["reorderLevel"];
-                    catalogue.ReorderQty = (int)reader["reorderQty"];
-                    catalogue.Category = (string)reader["category"];
-                    catalogue.Description = (string)reader["description"];
-                    catalogue.UnitOfMeasure = (string)reader["unitOfMeasure"];
-                    catalogue.ImageUrl = (string)reader["imageUrl"];
+                    catalogue = new Inventory()
+                    {
+                        ItemId = (int)reader["itemId"],
+                        ItemCode = (string)reader["itemCode"],
+                        BinNo = (reader["binNo"] == DBNull.Value) ? "Nil" : (string)reader["binNo"],
+                        StockLevel = (int)reader["stockLevel"],
+                        ReorderLevel = (int)reader["reorderLevel"],
+                        ReorderQty = (int)reader["reorderQty"],
+                        Category = (string)reader["category"],
+                        Description = (string)reader["description"],
+                        UnitOfMeasure = (string)reader["unitOfMeasure"],
+                        ImageUrl = (reader["imageUrl"] == DBNull.Value) ? "Nil" : (string)reader["imageUrl"]
+                    };
                 }
             }
             return catalogue;
         }
 
 
-        public List<string> DisplayAllCategory()
+        public static List<Inventory> DisplaySearchedCatalogue(string Description)
+        {
+            List<Inventory> catalogues = new List<Inventory>();
+
+            using (SqlConnection conn = new SqlConnection(Data.db_cfg))
+            {
+                conn.Open();
+
+                string q = @"SELECT * from Inventory WHERE description LIKE '%" + Description + "%'";
+                SqlCommand cmd = new SqlCommand(q, conn);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Inventory catalogue = new Inventory()
+                    {
+                        ItemId = (int)reader["itemId"],
+                        ItemCode = (string)reader["itemCode"],
+                        BinNo = (reader["binNo"] == DBNull.Value) ? "Nil" : (string)reader["binNo"],
+                        StockLevel = (int)reader["stockLevel"],
+                        ReorderLevel = (int)reader["reorderLevel"],
+                        ReorderQty = (int)reader["reorderQty"],
+                        Category = (string)reader["category"],
+                        Description = (string)reader["description"],
+                        UnitOfMeasure = (string)reader["unitOfMeasure"],
+                        ImageUrl = (reader["imageUrl"] == DBNull.Value) ? "Nil" : (string)reader["imageUrl"]
+                    };
+                    catalogues.Add(catalogue);
+                }
+            }
+            return catalogues;
+        }
+
+
+        public static List<string> DisplayAllCategory()
         {
             List<string> categories = new List<string>();
 
@@ -96,7 +133,7 @@ namespace SSISTeam9.DAO
         }
 
 
-        public List<string> DisplayAllUnit()
+        public static List<string> DisplayAllUnit()
         {
             List<string> units = new List<string>();
 
@@ -131,33 +168,33 @@ namespace SSISTeam9.DAO
         }
 
 
-        public static void CreateCatalogue(string ItemCode, string Category, string Description, string UnitOfMeasure, string Supplier1Id, string Supplier2Id, string Supplier3Id)
-        {
-            using (SqlConnection conn = new SqlConnection(Data.db_cfg))
-            {
-                conn.Open();
+        //public static void CreateCatalogue(string ItemCode, string Category, string Description, string UnitOfMeasure, string Supplier1Id, string Supplier2Id, string Supplier3Id)
+        //{
+        //    using (SqlConnection conn = new SqlConnection(Data.db_cfg))
+        //    {
+        //        conn.Open();
 
-                string q1 = @"SELECT MAX(itemId) from Inventory";
-                SqlCommand cmd1 = new SqlCommand(q1, conn);
-                int ItemId = (int)cmd1.ExecuteScalar() + 1;
+        //        string q1 = @"SELECT MAX(itemId) from Inventory";
+        //        SqlCommand cmd1 = new SqlCommand(q1, conn);
+        //        int ItemId = (int)cmd1.ExecuteScalar() + 1;
 
-                string q2 = "INSERT INTO Inventory (itemId,itemCode,category,description,unitOfMeasure,supplier1Id,supplier2Id,supplier3Id)" + "VALUES ('" + ItemId + "','" + ItemCode + "','" + Category + "','" + Description + "','" + UnitOfMeasure + "','" + Supplier1Id + "','" + Supplier2Id + "','" + Supplier3Id + "')";
-                SqlCommand cmd2 = new SqlCommand(q2, conn);
-                cmd2.ExecuteNonQuery();
-            }
-        }
+        //        string q2 = "INSERT INTO Inventory (itemId,itemCode,category,description,unitOfMeasure,supplier1Id,supplier2Id,supplier3Id)" + "VALUES ('" + ItemId + "','" + ItemCode + "','" + Category + "','" + Description + "','" + UnitOfMeasure + "','" + Supplier1Id + "','" + Supplier2Id + "','" + Supplier3Id + "')";
+        //        SqlCommand cmd2 = new SqlCommand(q2, conn);
+        //        cmd2.ExecuteNonQuery();
+        //    }
+        //}
 
 
-        public static void UpdateCatagolue(string ItemId, string Description, string UnitOfMeasure, string Supplier1Id, string Supplier2Id, string Supplier3Id)
-        {
-            using (SqlConnection conn = new SqlConnection(Data.db_cfg))
-            {
-                conn.Open();
+        //public static void UpdateCatagolue(string ItemId, string Description, string UnitOfMeasure, string Supplier1Id, string Supplier2Id, string Supplier3Id)
+        //{
+        //    using (SqlConnection conn = new SqlConnection(Data.db_cfg))
+        //    {
+        //        conn.Open();
 
-                string q = @"UPDATE Inventory SET description = '" + Description + "', unitOfMeasure = '" + UnitOfMeasure + "', supplier1Id = '" + Supplier1Id + "', supplier2Id = '" + Supplier2Id + "', supplier3Id = '" + Supplier3Id + " WHERE itemId ='" + ItemId +"'";
-                SqlCommand cmd = new SqlCommand(q, conn);
-                cmd.ExecuteNonQuery();
-            }
-        }
+        //        string q = @"UPDATE Inventory SET description = '" + Description + "', unitOfMeasure = '" + UnitOfMeasure + "', supplier1Id = '" + Supplier1Id + "', supplier2Id = '" + Supplier2Id + "', supplier3Id = '" + Supplier3Id + " WHERE itemId ='" + ItemId +"'";
+        //        SqlCommand cmd = new SqlCommand(q, conn);
+        //        cmd.ExecuteNonQuery();
+        //    }
+        //}
     }
 }

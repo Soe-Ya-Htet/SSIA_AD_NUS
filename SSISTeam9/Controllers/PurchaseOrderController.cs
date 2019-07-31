25687f87ea8e84a4fac6375db7e5064df9e220bb
Seetoh <seetohyongqiang@gmail.com>
2019-07-31 22:14:04 +0800

    Added new files for Purchase Order

---
+++ SSISTeam9/Controllers/PurchaseOrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSISTeam9.Controllers
+{
+    public class PurchaseOrderController : Controller
+    {
+        // GET: PurchaseOrder
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

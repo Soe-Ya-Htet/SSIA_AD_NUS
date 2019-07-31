bf5711737dbcadb6d8bab61fe861b4d3cc87430f
Liran Wang <e0385291@u.nus.edu>
2019-07-31 11:06:58 +0800

    Add DAO

---
+++ SSISTeam9/DAO/CatalogueDAO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using SSISTeam9.Models;
+
+namespace SSISTeam9.DAO
+{
+    public class CatalogueDAO
+    {
+        public List<Inventory> DisplayCatalogue()
+        {
+            List<Inventory> catalogues = new List<Inventory>();
+
+            using (SqlConnection conn = new SqlConnection(Data.db_cfg))
+            {
+                conn.Open();
+
+                string q = @"SELECT * from Inventory";
+                SqlCommand cmd = new SqlCommand(q, conn);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Inventory catalogue = new Inventory()
+                    {
+                        ItemId = (int)reader["itemId"],
+                        ItemCode = (string)reader["itemCode"],
+                        BinNo = (string)reader["binNo"],
+                        StockLevel = (int)reader["stockLevel"],
+                        ReorderLevel = (int)reader["reorderLevel"],
+                        ReorderQty = (int)reader["reorderQty"],
+                        Category = (string)reader["category"],
+                        Description = (string)reader["description"],
+                        UnitOfMeasure = (string)reader["unitOfMeasure"],
+                        ImageUrl = (string)reader["imageUrl"]
+                    };
+                    catalogues.Add(catalogue);
+                }
+            }
+            return catalogues;
+        }
+
+        public static void DeleteCatalogue(int ItemId)
+        {
+            using (SqlConnection conn = new SqlConnection(Data.db_cfg))
+            {
+                conn.Open();
+                string q = @"Update UserInfo set SessionID = NULL where SessionID ='"  + "'";
+                SqlCommand cmd = new SqlCommand(q, conn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
---
+++ SSISTeam9/DAO/DepartmentDAO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using SSISTeam9.Models;
+
+namespace SSISTeam9.DAO
+{
+    public class DepartmentDAO
+    {
+        public List<Department> DisplayAllSuppliers()
+        {
+            List<Department> departments = new List<Department>();
+
+            using (SqlConnection conn = new SqlConnection(Data.db_cfg))
+            {
+                conn.Open();
+
+                string q = @"SELECT * from Department";
+                SqlCommand cmd = new SqlCommand(q, conn);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Department department = new Department()
+                    {
+                        DeptId = (int)reader["deptId"],
+                        DeptCode = (string)reader["deptCode"],
+                        RepresentativeId = (int)reader["representativeId"],
+                        CollectionPointId = (int)reader["collectionPointId"],
+                        Name = (string)reader["name"],
+                        Contact = (string)reader["contact"],
+                        Telephone = (string)reader["telephone"],
+                        Fax = (string)reader["fax"],
+                        Head = (string)reader["head"]
+                    };
+                    departments.Add(department);
+                }
+            }
+            return departments;
+        }
+    }
+}

32cefaff5d94ac8bf46b44279d2b5289b801e7e5
Seah Shih Wei Gerome <e0321133@u.nus.edu>
2019-07-31 14:23:10 +0800

    added dao's

---
+++ SSISTeam9/Controllers/AdjVoucherController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSISTeam9.Controllers
+{
+    public class AdjVoucherController : Controller
+    {
+        // GET: AdjVoucher
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

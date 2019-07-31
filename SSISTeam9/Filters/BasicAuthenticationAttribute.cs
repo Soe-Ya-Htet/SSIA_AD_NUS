b3f9280a5ca1245418fcfd15fb4099f00152ae4d
Ravi <ravindrab749@gmail.com>
2019-07-31 14:04:03 +0800

    update

---
+++ SSISTeam9/Filters/BasicAuthenticationAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace SSISTeam9
+{
+    public class BasicAuthenticationAttribute : ActionFilterAttribute
+    {
+        public string BasicRealm { get; set; }
+        protected string Username { get; set; }
+        protected string Password { get; set; }
+
+        public BasicAuthenticationAttribute(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var request = filterContext.HttpContext.Request;
+            var auth = request.Headers["Authorization"];
+            if(String.IsNullOrEmpty(auth))
+            {
+                //filterContext.HttpContext.Response.addHeader("WWW-Authenticate", String.Format("Basic realm=\"{0}\"", BasicRealm ?? "Basic"));
+                filterContext.HttpContext.Response.addHeader("Authorization", String.Format("Basic " + Convert.ToBase64String(Encoding.Default.GetBytes("un:pw"), BasicRealm ?? "Basic")));
+                filterContext.HttpContext.Response.SuppressFormsAuthenticationRedirect = true; filterContext.HttpContext.Response.addHeader("WWW-Authenticate", String.Format("Basic realm=\"{0}\"", BasicRealm ?? "Basic"));
+
+                filterContext.Result = new HttpUnauthorizedResult();
+                return;
+            }
+
+            var cred = System.Text.ASCIIEncoding.ASCII.GetString(Convert.FromBase64String(auth.Substring(6))).Split(":");
+            var user = new { Name = cred[0], Pass = cred[1] };
+
+        }
+    }
+}

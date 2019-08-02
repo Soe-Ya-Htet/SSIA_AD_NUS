3b76bfe5b96e845161d0fda1695dbe04c7335516
Liran Wang <e0385291@u.nus.edu>
2019-08-02 12:03:00 +0800

    update Inventory model

--- SSISTeam9/Models/Inventory.cs
+++ SSISTeam9/Models/Inventory.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace SSISTeam9.Models
 {
     public class Inventory
     {
         public long ItemId { get; set; }
+
+        [Display(Name = "Item Code")]
+        [Required]
         public string ItemCode { get; set; }
         public string BinNo { get; set; }
         public int StockLevel { get; set; }
         public int ReorderLevel { get; set; }
         public int ReorderQty { get; set; }
+
+        [Display(Name = "Category")]
+        [Required]
         public string Category { get; set; }
+
+        [Display(Name = "Description")]
+        [Required]
         public string Description { get; set; }
+
+        [Display(Name = "Unit Of Measure")]
+        [Required]
         public string UnitOfMeasure { get; set; }
         public string ImageUrl { get; set; }
         public bool isChecked { get; set; }

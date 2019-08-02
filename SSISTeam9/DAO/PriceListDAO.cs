7b79fc299c9da0261d6e50806083e692307db555
Liran Wang <e0385291@u.nus.edu>
2019-08-02 15:20:14 +0800

    Add one view, Update price list DAO, service and supplier DAO to accept search by string

--- SSISTeam9/DAO/PriceListDAO.cs
+++ SSISTeam9/DAO/PriceListDAO.cs
@@ -26,15 +26,14 @@ namespace SSISTeam9.DAO
                 {
                     pricelist = new PriceList()
                     {                        
-                        Supplier1Id = (reader["supplier1Id"] == DBNull.Value) ? "Nil" : (string)reader["supplier1Id"],
-                        Supplier2Id = (reader["supplier2Id"] == DBNull.Value) ? "Nil" : (string)reader["supplier2Id"],
-                        Supplier3Id = (reader["supplier3Id"] == DBNull.Value) ? "Nil" : (string)reader["supplier3Id"],
+                        Supplier1Id = (string)reader["supplier1Id"],
+                        Supplier2Id = (string)reader["supplier2Id"],
+                        Supplier3Id = (string)reader["supplier3Id"],
                         Supplier1UnitPrice = (reader["supplier1UnitPrice"] == DBNull.Value) ? "Nil" : (string)reader["supplier1UnitPrice"],
                         Supplier2UnitPrice = (reader["supplier2UnitPrice"] == DBNull.Value) ? "Nil" : (string)reader["supplier2UnitPrice"],
                         Supplier3UnitPrice = (reader["supplier3UnitPrice"] == DBNull.Value) ? "Nil" : (string)reader["supplier3UnitPrice"]
                     };
                 }
-                reader.Close();
             }
             return pricelist;
         }

3886634960ee04f467ad73f7148c55df5c22cd78
Ravi <ravindrab749@gmail.com>
2019-08-04 12:48:03 +0800

    email base file added

---
+++ SSISTeam9/App_Start/UnityConfig.cs
@@ -0,0 +1,20 @@
+using SSISTeam9.Services;
+using System.Web.Mvc;
+using Unity;
+using Unity.Mvc5;
+
+namespace SSISTeam9
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+			var container = new UnityContainer();
+
+            // register all your components with the container here
+            container.RegisterType<IEmailService, EmailService>();
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
---
+++ SSISTeam9/Services/EmailService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace SSISTeam9.Services
+{
+    public class EmailService : IEmailService
+    {
+        private MailAddress senderEmail;
+        private SmtpClient smtp;
+        public EmailService()
+        {
+            senderEmail = new MailAddress("team9rockz@gmail.com", "Team9");
+            var password = "mylovelyteam";
+            smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(senderEmail.Address, password)
+            };
+        }
+        public void SendEmail(String receiverEmailAddress)
+        {
+            var subject = "Subject";
+            var body = "Body";
+
+            var receiverEmail = new MailAddress(receiverEmailAddress);
+            using (var mess = new MailMessage(senderEmail, receiverEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+            {
+                smtp.Send(mess);
+            }
+        }
+    }
+}

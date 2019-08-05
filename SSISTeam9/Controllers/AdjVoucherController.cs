b78e0b48e852cca99127c931baad344d4125aa96
Seah Shih Wei Gerome <e0321133@u.nus.edu>
2019-08-05 18:03:58 +0800

    Revert "adj voucher new files"
    This reverts commit d44f24879078248e96c214e4f156365145886653.

--- SSISTeam9/Controllers/AdjVoucherController.cs
+++ SSISTeam9/Controllers/AdjVoucherController.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using SSISTeam9.Models;
-using SSISTeam9.Services;
 
 namespace SSISTeam9.Controllers
 {
     public class AdjVoucherController : Controller
     {
         // GET: AdjVoucher
-        public ActionResult Index(List<AdjVoucher> adjVouchers)
+        public ActionResult Index()
         {
-            AdjVoucherService.InsertReason(adjVouchers);
             return View();
         }
     }

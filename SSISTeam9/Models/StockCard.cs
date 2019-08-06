5ec25b7efb0cccf54b3a3b5217ad762a76ab4819
Liran Wang <e0385291@u.nus.edu>
2019-08-06 20:06:29 +0800

    create stock card

---
+++ SSISTeam9/Models/StockCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SSISTeam9.Models
+{
+    public class StockCard
+    {
+        public long itemId { get; set; }
+        public DateTime Date { get; set; }
+        public string Source { get; set; }
+        public string Qty { get; set; }
+        public string Balance { get; set; }
+    }
+}

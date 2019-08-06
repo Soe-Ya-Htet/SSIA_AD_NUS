ab9f29ffc25a5d0a70eee09c443ad5d396ad7814
Liran Wang <e0385291@u.nus.edu>
2019-08-06 23:18:29 +0800

    Added StockCard and ChargeBack entity and DAO

---
+++ SSISTeam9/Models/ChargeBack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SSISTeam9.Models
+{
+    public class ChargeBack
+    {
+        public long Id { get; set; }
+        public int MonthOfOrder { get; set; }
+        public int YearOfOrder { get; set; }
+        public Department Department { get; set; }
+        public int Amount { get; set; }
+    }
+}

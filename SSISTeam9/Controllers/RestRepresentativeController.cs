e9e8c81114d215c59b31152b651ec8497d192385
Ravi <ravindrab749@gmail.com>
2019-08-10 17:54:49 +0800

    new files

---
+++ SSISTeam9/Controllers/RestRepresentativeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSISTeam9.Controllers
+{
+    [RoutePrefix("rest/representative")]
+    public class RestRepresentativeController : Controller
+    {
+        [Route("items/received")]
+        public ActionResult Index()
+        {
+            return Json("", JsonRequestBehavior.AllowGet);
+        }
+    }
+}

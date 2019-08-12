9bbb61d0975317449d2a5d1a859fae0ae7771720
Liran Wang <e0385291@u.nus.edu>
2019-08-12 17:08:05 +0800

    Add session Id for chargeback and stock card

--- SSISTeam9/Controllers/StockCardController.cs
+++ SSISTeam9/Controllers/StockCardController.cs
@@ -5,12 +5,14 @@ using System.Web;
 using System.Web.Mvc;
 using SSISTeam9.Models;
 using SSISTeam9.Services;
+using SSISTeam9.Filters;
 
 namespace SSISTeam9.Controllers
 {
     public class StockCardController : Controller
     {
-        public ActionResult StockCard(long itemId)
+        [StoreAuthorisationFilter]
+        public ActionResult StockCard(long itemId, string sessionId)
         {
                
             ViewData["catalogue"] = CatalogueService.GetCatalogueById(itemId);

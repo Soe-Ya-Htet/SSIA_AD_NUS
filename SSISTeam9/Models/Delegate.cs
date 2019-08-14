4c78ee9b11fa170814798f5a967fc681873d2ad4
Nway Sandi Oo <e0395885@u.nus.edu>
2019-08-14 17:39:28 +0800

    Some changes in views

--- SSISTeam9/Models/Delegate.cs
+++ SSISTeam9/Models/Delegate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace SSISTeam9.Models
 {
     public class Delegate
     {
+        [Display(Name = "From")]
         public DateTime FromDate { get; set; }
+        [Display(Name = "To")]
         public DateTime ToDate { get; set; }
+        [Display(Name = "Employee Name")]
         public Employee Employee { get; set; }
         public Department Department { get; set; }
     }

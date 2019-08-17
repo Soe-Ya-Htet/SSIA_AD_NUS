f1fe5cf1b7898f44659ca2bc388a6c331661d69b
Liran Wang <e0385291@u.nus.edu>
2019-08-17 17:49:06 +0800

    update adj voucher controller for stock card

--- SSISTeam9/Controllers/AdjVoucherController.cs
+++ SSISTeam9/Controllers/AdjVoucherController.cs
@@ -13,53 +13,7 @@ namespace SSISTeam9.Controllers
     [StoreAuthorisationFilter]
     public class AdjVoucherController : Controller
     {
-        //[HttpPost]
-        //public ActionResult Index(List<Inventory> inventories)
-        //{
-        //    if (inventories == null)
-        //    {
-        //        return RedirectToRoute(new {controller = "Stock", action = "Check"});
-        //    }
-        //    List<AdjVoucher> adjVouchers = new List<AdjVoucher>();
-
-        //    foreach (Inventory inventory in inventories)
-        //    {
-        //        int qty = inventory.ActualStock - inventory.StockLevel;
-        //        if (qty != 0)
-        //        {
-        //            PriceList list = PriceListDAO.GetPriceListById(inventory.ItemId) ?? new PriceList();
-        //            inventory.ItemSuppliersDetails = list;
-        //            AdjVoucher adjVoucher = new AdjVoucher
-        //            {
-        //                AdjQty = qty,
-        //                Item = inventory,
-        //                TotalPrice = inventory.ItemSuppliersDetails.Supplier1UnitPrice * qty
-        //            };
-        //            adjVouchers.Add(adjVoucher);
-        //        }
-        //    }
-
-        //    return View(adjVouchers);
-        //}
-
-        //public ActionResult Generate(List<AdjVoucher> adjVouchers)
-        //{
-        //    int managerId = 14;
-        //    int supervisorId = 12;
-        //    if (adjVouchers != null && adjVouchers.Count > 0)
-        //    {
-        //        for (var i = 0; i < adjVouchers.Count; i++)
-        //        {
-        //            int id = (adjVouchers[i].TotalPrice < 250.0) ? supervisorId : managerId;
-        //            adjVouchers[i].AuthorisedBy = id.ToString();
-        //        }
-        //        AdjVoucherDAO.GenerateDisbursement(adjVouchers);
-        //        AdjVoucherDAO.UpdateStock(adjVouchers);
-        //    }
-
-        //    ViewBag.Msg = "Success";
-        //    return View(new List<AdjVoucher>());
-        //}
+        
 
         public ActionResult Index(string sessionId)
         {
